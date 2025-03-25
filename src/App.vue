a5c97ae37e52bf63ee70dacdbd88646da133fe5c
br4hm <haehnlein.alexander@gmx.de>
2025-03-25 15:18:36 +0100

    create first component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import BaseCard from "@/components/BaseCard.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="content">
+    <BaseCard></BaseCard>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <BaseCard></BaseCard>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+  .content {
+    width: 80%;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    flex-direction: column;
+    gap:24px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card">
+    <slot>Hello?</slot>
+  </div>
+</template>
+
+<style scoped>
+  .card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 24px 32px;
+    width: 100%;
+  }
+</style>
